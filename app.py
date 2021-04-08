6cc15c3725bf6ce4186bbe27100d4c380ef644a6
Naman Agrawal <na77@njit.edu>
2021-04-08 18:45:05 +0000

    Added REST API demo code from Lecture 22

--- app.py
+++ app.py
@@ -1,14 +1,64 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, jsonify, Response
 
 app = Flask(__name__, static_folder='./build/static')
 
+# Mock data, in reality you would have a database
+# List of dictionaries is a great JSON format to return data
+CATALOG = [
+    {
+        'id': 1,
+        'title': 'The Reluctant Fundamentalist',
+        'author': 'Mohsin Hamid',
+    },
+    {
+        'id': 2,
+        'title': 'Sea of Poppies',
+        'author': 'Amitav Ghosh',
+    },
+    {
+        'id': 3,
+        'title': 'Debt',
+        'author': 'David Graeber',
+    },
+]
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/books/all', methods=['GET'])
+def api_all():
+    return jsonify(CATALOG)
+
+# A route to create or access a specific entry in our catalog based on request.
+@app.route('/api/v1/books', methods=['GET', 'POST'])
+def api_id():
+    # User wants to create a new book in the catalog
+    if request.method == 'POST':
+        # Gets the JSON object from the body of request sent by client
+        request_data = request.get_json()
+        new_book = {
+            'id': len(CATALOG),
+            'title': request_data['title'],
+            'author': request_data['author'],
+        }
+        CATALOG.append(new_book)
+        return {'success': True} # Return success status if it worked
+    else:
+        book_id = request.args.get('book_id', '')
+        if book_id == '':
+            return Response("Error: No id field provided. Please specify an id.", status=400)
+
+        # For real DB, you would replace with a filter clause in SQLAlchemy
+        results = []
+        for book in CATALOG:
+            if book['id'] == int(book_id):
+                results.append(book)
+
+    return jsonify(results)
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from 'react';
+
+const BASE_URL = '/api/v1/books';
 
 function App() {
+  // For storing info fetched from server
+  const [books, setBooks] = useState([]);
+  const [createStatus, setCreateStatus] = useState(false);
+
+  // Store reference to input elements to access typed in values
+  const idRef = useRef(null);
+  const titleRef = useRef(null);
+  const authorRef = useRef(null);
+
+  // Fetch all books when you first load the page without clicking button (optional)
+  // useEffect(() => {
+  //   fetch('/api/v1/books/all', {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //     })
+  //     .then(response => {
+  //       return response.json(); // Convert to json
+  //     }).then(responseData => {
+  //       // Whatever you want to do with the data returned by server
+  //       setBooks(responseData);
+  //     });
+  // }, []);
+
+  function fetchAllBooks() {
+    fetch('/api/v1/books/all', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      .then(response => {
+        return response.json(); // Convert to json
+      }).then(responseData => {
+        // Whatever you want to do with the data returned by server
+        setBooks(responseData);
+      });
+  }
+
+  function fetchBookByID() {
+    const idValue = idRef.current.value;
+    const url = BASE_URL + '?book_id=' + idValue; // Add query parameter id, can add multiple with &
+    fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      .then(response => {
+        return response.json();
+      }).then(responseData => {
+        // Whatever you want to do with the data returned by server
+        setBooks(responseData);
+      });
+  }
+
+  function createBook() {
+    setCreateStatus(false);
+    const titleValue = titleRef.current.value;
+    const authorValue = authorRef.current.value;
+
+    const data = JSON.stringify({
+      'title': titleValue,
+      'author': authorValue,
+    });
+    fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: data, // No query parameter, for POST we put in body
+      })
+      .then(response => {
+        return response.json();
+      }).then(responseData => {
+        // Whatever you want to do with the data returned by server
+        setCreateStatus(responseData.success);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Book API Demo</h1>
+      <div>
+        <button onClick={fetchAllBooks}>Fetch All Books</button>
+      </div>
+      <div>
+        <input ref={idRef} type="text" placeholder="Enter book ID" />
+        <button onClick={fetchBookByID}>Fetch Specific Book</button>
+      </div>
+      <div>
+        <input ref={titleRef} type="text" placeholder="Enter book title" />
+        <input ref={authorRef} type="text" placeholder="Enter book author" />
+        <button onClick={createBook}>Create New Book</button>
+        {createStatus ? "Successfully created book!" : null}
+      </div>
+      <ul>
+        {books.map(book => <li><b>{book.title}</b> by {book.author} (id {book.id})</li>)}
+      </ul>
     </div>
   );
 }
